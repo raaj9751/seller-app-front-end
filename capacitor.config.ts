d3c01a3d464b54cf048f14768e14dfab870bc474
piratesfair@gmail.com <sankar@imcapsule.io>
2024-05-06 17:14:46 +0530

    chnages loo and gaut

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,20 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'CW Mart',
-  webDir: 'dist',
+  appId: "io.ionic.starter",
+  appName: "CW Mart",
+  webDir: "dist",
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: "https",
+  },
+  plugins: {
+    GoogleAuth: {
+      scopes: ["profile", "email"],
+      serverClientId:
+        "471807870049-ulek4vks6u3fm10tgqghid06vqu0c19g.apps.googleusercontent.com",
+      forceCodeForRefreshToken: true,
+    },
+  },
 };
 
 export default config;
